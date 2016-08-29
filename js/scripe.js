cfd8f295034b0cf99a6fae2c8876c950a93b7541
Gigaiot <916238589@qq.com>
2016-08-29 17:44:27 +0800

    160829_1

---
+++ js/scripe.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+	var page1 = document.getElementById('page_1');
+	var page2 = document.getElementById('page_2');
+	var page3 = document.getElementById('page_3');
+
+	var music = document.getElementById('music');
+}
